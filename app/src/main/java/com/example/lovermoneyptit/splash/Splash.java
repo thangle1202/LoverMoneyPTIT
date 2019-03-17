46b650f64d9a9833b0afa8673578958a82fa184c
thuannd97 <nguyenducthuan091997@gmail.com>
2019-03-17 18:33:09 +0700

    add splash activity

---
+++ app/src/main/java/com/example/lovermoneyptit/splash/Splash.java
@@ -0,0 +1,27 @@
+package com.example.lovermoneyptit.splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lovermoneyptit.HomeFragment;
+import com.example.lovermoneyptit.MainActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
